366ba71387dbcd7e79efa847cc73bfa073ceeb7c
Ronald Tagolino <tags0516@gmail.com>
2017-01-27 07:53:03 +0000

    Lualu set page to 99. Scraping article page for hilive.

--- hilive/hilive/spiders/scrape_hilive.py
+++ hilive/hilive/spiders/scrape_hilive.py
@@ -11,27 +11,29 @@ class ScrapeHiliveSpider(scrapy.Spider):
     def parse(self, response):
         html = BeautifulSoup(response.body, "html.parser")
 
-        print(response.url)
-        print(len(html.findAll('div', {'class': "newsblock"})))
-        for newsblock in html.findAll('div', {'class': "newsblock"}):
-            news = {}
-            news['title'] = newsblock.find("h2").find("a").string
-            news['thumbnail_url'] = "{}{}".format(self.allowed_domains[0], newsblock.find("img")['src'])
-            news['image_urls'] = []
-            news['image_urls'].append("https://{}{}".format(self.allowed_domains[0], newsblock.find("img")['src']))
-            news['article_url'] = "https://{}{}".format(self.allowed_domains[0], newsblock.find("h2").find("a")['href'])
-            articles = yield scrapy.Request(news['article_url'], meta={'news': news}, callback=self.scrape_hiliveArticle)
-            news['articles'] = articles
-            yield news
+    print(response.url)
+    print(len(html.findAll('div', {'class': "newsblock"})))
+    for newsblock in html.findAll('div', {'class': "newsblock"}):
+        news = {}
+        news['title'] = newsblock.find("h2").find("a").string
+        news['thumbnail_url'] = "{}{}".format(self.allowed_domains[0], newsblock.find("img")['src'])
+        news['image_urls'] = []
+        news['image_urls'].append("https://{}{}".format(self.allowed_domains[0], newsblock.find("img")['src']))
+        news['article_url'] = "https://{}{}".format(self.allowed_domains[0], newsblock.find("h2").find("a")['href'])
+        articles = yield scrapy.Request(news['article_url'], meta={'news': news}, callback=self.scrape_hiliveArticle)
+        news['articles'] = articles
+        yield news
+        return None
 
-            return None
-        if html.find('a', {'id': "NextPage"}) and 'href' in html.find('a', {'id': "NextPage"}):
-            yield scrapy.Request("https://{}{}".format(self.allowed_domains[0], html.find('a', {'id': "NextPage"})['href']), self.parse)
+    if html.find('a', {'id': "NextPage"}) and 'href' in html.find('a', {'id': "NextPage"}):
+        yield scrapy.Request("https://{}{}".format(self.allowed_domains[0], html.find('a', {'id': "NextPage"})['href']), self.parse)
 
 
     def scrape_hiliveArticle(self, response):
         news = response.meta['news']
         news['articles'] = []
+        html = BeautifulSoup(response.body, "html.parser")
 
+        for d in html.find('div', {'class': "bodytxt"}).findAll("div"):
+            print(d)
         return news
-
