fdf34beb0639ddd7956384ad800eb7259c802708
Ronald Tagolino <tags0516@gmail.com>
2017-01-27 03:47:52 +0000

    Scraping hilive. Working on articles.

--- hilive/hilive/spiders/scrape_hilive.py
+++ hilive/hilive/spiders/scrape_hilive.py
@@ -14,8 +14,24 @@ class ScrapeHiliveSpider(scrapy.Spider):
         print(response.url)
         print(len(html.findAll('div', {'class': "newsblock"})))
         for newsblock in html.findAll('div', {'class': "newsblock"}):
-        	print(newsblock.find("h2").find("a").string)
+            news = {}
+            news['title'] = newsblock.find("h2").find("a").string
+            news['thumbnail_url'] = "{}{}".format(self.allowed_domains[0], newsblock.find("img")['src'])
+            news['image_urls'] = []
+            news['image_urls'].append("https://{}{}".format(self.allowed_domains[0], newsblock.find("img")['src']))
+            news['article_url'] = "https://{}{}".format(self.allowed_domains[0], newsblock.find("h2").find("a")['href'])
+            articles = yield scrapy.Request(news['article_url'], meta={'news': news}, callback=self.scrape_hiliveArticle)
+            news['articles'] = articles
+            yield news
 
-        if html.find('a', {'id': "NextPage"}):
-        	yield scrapy.Request("https://{}{}".format(self.allowed_domains[0], html.find('a', {'id': "NextPage"})['href']), self.parse)
+            return None
+        if html.find('a', {'id': "NextPage"}) and 'href' in html.find('a', {'id': "NextPage"}):
+            yield scrapy.Request("https://{}{}".format(self.allowed_domains[0], html.find('a', {'id': "NextPage"})['href']), self.parse)
+
+
+    def scrape_hiliveArticle(self, response):
+        news = response.meta['news']
+        news['articles'] = []
+
+        return news
 
