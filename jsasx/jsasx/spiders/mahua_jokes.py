fc4fa83eabe8d42b631ba80a2fd0e497565ac69c
Ronald Tagolino <tags0516@gmail.com>
2017-02-13 10:42:23 +0000

    Adjust xpath for joke content.

--- jsasx/jsasx/spiders/mahua_jokes.py
+++ jsasx/jsasx/spiders/mahua_jokes.py
@@ -21,12 +21,16 @@ class MahuaJokesSpider(scrapy.Spider):
 			item['thumbnail_url'] = img_tag.xpath("@src").extract_first() if img_tag.xpath("@src").extract_first() else img_tag.xpath("@mahuaimg").extract_first()
 			item['article_url'] = dl.xpath("""./dt/span/a/@href""").extract_first()
 			search_response = requests.get("{}posts?search={}".format(API_BASE_URL, item['title']))
+			skipthis = False
 			if search_response.status_code == 200:
 				if search_response.json():
 					for post in search_response.json():
 						if item['title'].strip() == post['title']['rendered'].strip():
 							print("{}   {}  {}".format("Title existed in API.", item['article_url'], item['title']))
-							continue
+							skipthis = True
+							break
+			if skipthis:
+				continue
 			item['articles'] = yield scrapy.Request(item['article_url'], meta={'item': item}, callback=self.parse_content)
 		next_page = response.xpath("""//div/a[contains(text(), "下一页")]""").xpath("@href").extract_first()
 		if next_page is not None:
@@ -43,6 +47,6 @@ class MahuaJokesSpider(scrapy.Spider):
 				item['articles'].append(content.xpath(".//img/@src").extract_first())
 				item['image_urls'].append(content.xpath(".//img/@src").extract_first())
 				item['image_only'] = True
-			elif content.xpath("./*/text()"):
-				item['articles'].extend([t.strip() for t in content.xpath("./*/text()").extract()])
+			elif content.xpath("""./div[@class="joke-content"]//text()"""):
+				item['articles'].extend([t.strip() for t in content.xpath("""./div[@class="joke-content"]//text()""").extract()])
 		yield item
